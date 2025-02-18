94185dd3068f50e9f42250b8ab99bf9638098047
smd995 <112795934+smd995@users.noreply.github.com>
2025-02-18 12:07:23 +0900

    husky test

--- playground/msw.test.ts
+++ playground/msw.test.ts
@@ -9,4 +9,14 @@ describe("msw", () => {
 
     console.log(data);
   });
+
+  it("post", async () => {
+    const result = await fetch("http://localhost:4000/contents", {
+      method: "POST",
+      body: JSON.stringify({ name: "test" }),
+    });
+    const data = await result.json();
+
+    console.log(data);
+  });
 });
